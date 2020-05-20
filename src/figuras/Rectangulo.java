83880129f0e409ac6e7edc6f0badf3f399bf050a
Ana Maria <anamaria.huguet.alum@iescamp.es>
2020-05-20 13:45:42 +0200

    Creacion de la clase 'Rectangulo' y el metodo 'obtenerArea'

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,22 @@
+// Ana Maria Huguet
+package figuras;
+
+/**
+ *
+ * @author Ana
+ */
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+    
+    public Rectangulo (double l1, double l2){
+        
+        lado1=l1;
+        lado2=l2;
+    }
+    
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+    
+}
